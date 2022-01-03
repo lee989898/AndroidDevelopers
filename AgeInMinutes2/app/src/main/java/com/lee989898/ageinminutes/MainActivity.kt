2fe9c1b8fb50ccb029ce720a9631755500bbbacd
lee989898 <wnsrud1352@gmail.com>
2022-01-03 17:43:30 +0900

    commit

--- AgeInMinutes2/app/src/main/java/com/lee989898/ageinminutes/MainActivity.kt
+++ AgeInMinutes2/app/src/main/java/com/lee989898/ageinminutes/MainActivity.kt
@@ -1,11 +1,59 @@
 package com.lee989898.ageinminutes
 
+import android.app.DatePickerDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import java.text.SimpleDateFormat
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
+
+    private var tvSelectedDate: TextView? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btnDatePicker: Button = findViewById(R.id.btnDatePicker)
+        tvSelectedDate = findViewById(R.id.tvSelectedDate)
+
+        btnDatePicker.setOnClickListener {
+            clickDatePicker()
+
+        }
+
     }
+
+    private fun clickDatePicker(){
+
+
+        val myCalender = Calendar.getInstance()
+        val year = myCalender.get(Calendar.YEAR)
+        val month = myCalender.get(Calendar.MONTH)
+        val day = myCalender.get(Calendar.DAY_OF_MONTH)
+        DatePickerDialog(this,
+            { view, selectedYear, selectedMonth, selectedDayOfMonth ->
+
+                Toast.makeText(this, "btnDatePicker pressed", Toast.LENGTH_LONG).show()
+
+                val selectedDate = "$selectedDayOfMonth/${selectedMonth+1}/$selectedYear"
+
+                tvSelectedDate?.text = (selectedDate)
+
+                val sdf = SimpleDateFormat("dd/MM/yyyy", Locale.ENGLISH)
+
+                val theDate = sdf.parse(selectedDate)
+
+
+            },
+            year,
+            month,
+            day
+            ).show()
+    }
+
 }
