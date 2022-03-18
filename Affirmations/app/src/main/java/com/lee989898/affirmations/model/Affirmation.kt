9f9a4333f276f3880ae53138b69d58acd0382032
lee989898 <wnsrud1352@gmail.com>
2022-03-18 09:47:17 +0900

    feat: 명언 앱

---
+++ Affirmations/app/src/main/java/com/lee989898/affirmations/model/Affirmation.kt
@@ -0,0 +1,8 @@
+package com.lee989898.affirmations.model
+
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+
+data class Affirmation(
+    @StringRes val stringResourceId: Int,
+    @DrawableRes val imageResourceId: Int)
